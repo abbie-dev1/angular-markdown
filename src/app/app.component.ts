dd273498577ba5242f1701fda423a7c8d3877618
abbie-dev1 <sitholeabbie@gmail.com>
2022-09-13 09:32:40 +0200

    markdown done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ToolbarService, LinkService, ImageService, HtmlEditorService } from '@syncfusion/ej2-angular-richtexteditor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+
+ 
+  styleUrls: ['./app.component.scss'],
+  providers: [ToolbarService, LinkService, ImageService, HtmlEditorService]
 })
 export class AppComponent {
   title = 'text-editor';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RichTextEditorModule } from '@syncfusion/ej2-angular-richtexteditor';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RichTextEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
